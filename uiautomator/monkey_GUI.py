fd72b8e2e1d4718345a839154ad34c4f6bdffd00
linao <justlinao@163.com>
2020-08-14 10:45:33 +0800

    微信窗口UI 自动化

--- uiautomator/monkey_GUI.py
+++ uiautomator/monkey_GUI.py
@@ -6,10 +6,8 @@
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import sys
 from monkey_command import monkey
